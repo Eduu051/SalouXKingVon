09ae219da73b6d9480703e130d82da7bbfd0bf94
DavidLucia15 <david.lucia@insbaixcamp.cat>
2025-10-01 17:36:55 +0200

    Update routes and add views for prueba and master layout

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+    Vista de Prueba    
+@endsection
+
+@section('content')
+    <h1>Hola, esta es una vista de prueba</h1>
+    <p>Este es un ejemplo de contenido en la vista de prueba.</p>
+@endsection
